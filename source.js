bcfb2c2a92184942869cec1ca00497a628c6a26c
Thinh <minhthinh0900@gmail.com>
2023-11-27 00:19:59 +0700

    Add grid 16x16 grid of square divs
    Using js to create 16 x 16 grid of squares divs and using css to make
    the grid visible

---
+++ source.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+for(let i = 0; i < 16; ++i)
+{
+    let row = document.createElement('div');
+    row.classList.add('row');
+    for(let j = 0; j < 16; ++j)
+    {
+        let column = document.createElement('div');
+        column.textContent = "";
+        column.classList.add('col');
+        row.appendChild(column);
+    }
+    container.appendChild(row);
+}
