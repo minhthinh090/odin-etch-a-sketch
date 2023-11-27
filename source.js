2edcb232614e927bdfeb92aff7b959eaa45c3750
Thinh <minhthinh0900@gmail.com>
2023-11-27 13:08:17 +0700

    add paint feature by hover and change size button

--- source.js
+++ source.js
@@ -1,14 +1,47 @@
-const container = document.querySelector('.container');
-for(let i = 0; i < 16; ++i)
+const grid = document.querySelector('.grid');
+function createGrid(size)
 {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    for(let j = 0; j < 16; ++j)
+    for(let i = 0; i < size; ++i)
     {
-        let column = document.createElement('div');
-        column.textContent = "";
-        column.classList.add('col');
-        row.appendChild(column);
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for(let j = 0; j < size; ++j)
+        {
+            let div = document.createElement('div');
+            div.textContent = "";
+            div.classList.add('col');
+            row.classList.add('row');
+            row.appendChild(div);
+            div.addEventListener('mouseover', () =>
+            {
+                div.style.backgroundColor = 'black';
+            })
+        }
+        grid.appendChild(row);
     }
-    container.appendChild(row);
 }
+let changeSize = document.querySelector('#changeSize');
+
+function removeGrid()
+{
+    let rows = document.querySelectorAll('.row')
+    rows.forEach(row => {
+        grid.removeChild(row);
+    })
+}
+
+changeSize.addEventListener('click', () => {    
+    let userInput = prompt('Please enter the size of grid you want: ');
+    if (+userInput > 0 && +userInput < 100 && +userInput != NaN)
+    {
+        removeGrid();
+        createGrid(+userInput);
+    }
+
+})
+
+
+
+
+
+createGrid(16);
